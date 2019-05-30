7b75a9b80a9643b7260d4031070e72a5a60eb32c
David Horwitz <dhorwitz@gmail.com>
2019-05-30 09:42:14 +0200

    NOJIRA import log ...

--- src/java/org/sakaiproject/profilesearch/logic/impl/OfficialPictureEntityProvider.java
+++ src/java/org/sakaiproject/profilesearch/logic/impl/OfficialPictureEntityProvider.java
@@ -1,15 +1,12 @@
 package org.sakaiproject.profilesearch.logic.impl;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.api.common.edu.person.SakaiPerson;
 import org.sakaiproject.api.common.edu.person.SakaiPersonManager;
+import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
 import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
 
 import lombok.extern.slf4j.Slf4j;
-
-import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
 
 @Slf4j
 public class OfficialPictureEntityProvider extends AbstractEntityProvider implements CoreEntityProvider,
